3e269901768b87e4052a7ae6e03951c0c15483c7
Caio Sanches <ccampato@gmail.com>
2018-06-05 22:32:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,11 +10,11 @@ class Song
   def initialize
     @@songs << self
   end
-
+'''
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
   end
-
+'''
   def artist=(artist)
     @artist = artist
   end
